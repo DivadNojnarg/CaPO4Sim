e29a13200d8b1f2f5b2023f5efa1effff9249afb
Divad0175 <dgranjon@ymail.com>
2019-02-19 14:47:27 +0100

    rework on modals entry level app

--- inst/entry_level/network_modals.R
+++ inst/entry_level/network_modals.R
@@ -1,16 +1,3 @@
-# *------------------------------------------------------------------
-# | PROGRAM NAME: network_modals.R
-# | DATE: 29/03/2018
-# | CREATED BY:  David Granjon
-# *----------------------------------------------------------------
-# | PURPOSE:  This code provide all modals needed to generate the
-# |           detailed zooms for  each case study
-# *-----------------------------------------------------------------
-# | UPDATES: 29/05/2018 (last update)
-# |
-# |
-# *------------------------------------------------------------------
-
 # custom top right button in the title
 # better position than at the bottom...
 top_dismiss_btn <- tags$button(
@@ -21,7 +8,12 @@ top_dismiss_btn <- tags$button(
 )
 
 
-# Modal img
+#' @title Create a modal image wrapper
+#'
+#' @description A modal image wrapper
+#'
+#' @param src Image path or url.
+#' @param size Image size. 440px by default.
 modal_img <- function(src, size = "440px") {
   a(
     href = src,
@@ -37,14 +29,22 @@ modal_img <- function(src, size = "440px") {
 }
 
 
-# generate modal
-generate_modal <- function(..., title, location, disease = NULL) {
+
+#' @title Generate a single modal
+#'
+#' @description Creates a single modal
+#'
+#' @param ... Modal content.
+#' @param title Modal title.
+#' @param location Organ location. Can be \code{c("intestine", "bones", "kidneys", "PTHg")}.
+#' @param diseases Current diseases. NULL or \code{c("php1", "hypopara", "hypoD3")}.
+generate_modal <- function(..., title, location, diseases = NULL) {
 
   # modal name
-  modalName <- if(is.null(disease)) {
+  modalName <- if(is.null(diseases)) {
     paste0("modal_zoom_", location)
   } else {
-    paste0("modal_zoom_", location, "_", disease)
+    paste0("modal_zoom_", location, "_", diseases)
   }
 
   # modal Tag
@@ -60,466 +60,175 @@ generate_modal <- function(..., title, location, disease = NULL) {
   assign(modalName, modalTag, envir = .GlobalEnv)
 }
 
-#-------------------------------------------------------------------------
-# Intestinal Mechanisms
-#-------------------------------------------------------------------------
 
-# baseline
-generate_modal(
-  title = "Detailed Baseline Intestinal Mechanisms",
-  location = "intestine",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      modal_img("base_case_zoom/intestine/base_case_notif_intestine.svg")
-    )
-  )
-)
-
-# php1
-generate_modal(
-  title = "Detailed Baseline Intestinal Mechanisms",
-  location = "intestine",
-  disease = "php1",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      modal_img("php1_zoom/intestine/php1_notif_intestine.svg")
-    )
-  )
-)
+diseases_ids <-  c("base_case", "php1", "hypopara", "hypoD3")
+locations <- c("intestine", "bones", "kidneys", "PTHg")
 
 
-# hypopara
-generate_modal(
-  title = "Detailed Baseline Intestinal Mechanisms",
-  location = "intestine",
-  disease = "hypopara",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      modal_img("hypopara_zoom/intestine/hypopara_notif_intestine.svg")
-    )
-  )
-)
+#' @title Generate all modals
+#'
+#' @description Creates a all modals (16)
+#'
+#' @param diseases Current diseases. NULL or \code{c("php1", "hypopara", "hypoD3")}.
+#' @param location Organ location. Can be \code{c("intestine", "bones", "kidneys", "PTHg")}.
+create_all_modals <- function(diseases, locations) {
+  lapply(seq_along(locations), FUN = function(i) {
+    lapply(seq_along(diseases), FUN = function(j) {
 
+      generate_modal(
+        title = if (diseases[[j]] == "base_case") {
+          paste("Detailed Baseline", locations[[i]], "Mechanisms")
+        } else if (diseases[[j]] == "php1") {
+          paste("Detailed", locations[[i]] , "Mechanisms During Primary Hyperparathyroidism")
+        } else if (diseases[[j]] == "hypopara") {
+          paste("Detailed", locations[[i]] , "Mechanisms During Hypoparathyroidism")
+        } else {
+          paste("Detailed", locations[[i]] , "Mechanisms During Vitamin D3 Deficiency")
+        },
+        location = locations[[i]],
+        diseases = if (j == 1) NULL else diseases[[j]],
+        if (locations[[i]] == "intestine") {
+          fluidRow(
+            column(
+              width = 12,
+              align = "center",
+              modal_img(paste0(diseases[[j]], "_zoom/intestine/", diseases[[j]], "_notif_intestine.svg"))
+            )
+          )
+        } else if (locations[[i]] == "bones") {
+          tagList(
+            fluidRow(
+              column(
+                width = 12,
+                align = "center",
+                "Effect of PTH on bone",
+                br(),
+                modal_img(paste0(diseases[[j]], "_zoom/bone/", diseases[[j]], "_notif_bone1.svg"))
+              )
+            ),
+            hr(),
+            fluidRow(
+              column(
+                width = 12,
+                align = "center",
+                "Effect of D3 on bone",
+                br(),
+                modal_img(paste0(diseases[[j]], "_zoom/bone/", diseases[[j]], "_notif_bone2.svg"))
+              )
+            )
+          )
+        } else if (locations[[i]] == "kidneys") {
+          tagList(
+            fluidRow(
+              column(
+                width = 6,
+                align = "center",
+                "Detailed Ca PT reabsorption",
+                br(),
+                modal_img(src = paste0(diseases[[j]], "_zoom/kidney/", diseases[[j]], "_notif_kidney1.svg"), size = "260px")
+              ),
+              column(
+                width = 6,
+                align = "center",
+                "Detailed Pi PT reabsorption",
+                br(),
+                modal_img(src = paste0(diseases[[j]], "_zoom/kidney/", diseases[[j]], "_notif_kidney2.svg"), size = "260px")
+              )
+            ),
+            hr(),
+            fluidRow(
+              column(
+                width = 6,
+                align = "center",
+                "Detailed Ca TAL reabsorption",
+                br(),
+                modal_img(src = paste0(diseases[[j]], "_zoom/kidney/", diseases[[j]], "_notif_kidney3.svg"), size = "260px")
+              ),
+              column(
+                width = 6,
+                align = "center",
+                "Detailed Ca DCT reabsorption",
+                br(),
+                modal_img(src = paste0(diseases[[j]], "_zoom/kidney/", diseases[[j]], "_notif_kidney4.svg"), size = "260px")
+              )
+            )
+          )
+        } else {
+          if (diseases[[j]] == "base_case") {
+            fluidRow(
+              column(
+                width = 12,
+                align = "center",
+                modal_img("base_case_zoom/PTHg/base_case_notif_PTHg.svg")
+              )
+            )
+          } else if (diseases[[j]] == "hypoD3") {
+            fluidRow(
+              column(
+                width = 12,
+                align = "center",
+                "Detailed PTH mechanisms",
+                br(),
+                modal_img(src = "hypoD3_zoom/PTHg/hypoD3_notif_PTHg1.svg", size = "260px")
+              )
+            )
+          } else {
+            tagList(
+              fluidRow(
+                column(
+                  width = 6,
+                  align = "center",
+                  "Detailed PTH mechanisms",
+                  br(),
+                  if (j == 2) {
+                    modal_img(src = "php1_zoom/PTHg/php1_notif_PTHg1.svg", size = "260px")
+                  } else {
+                    modal_img(src = "hypopara_zoom/PTHg/hypopara_notif_PTHg1.svg", size = "260px")
+                  }
+                ),
+                column(
+                  width = 6,
+                  align = "center",
+                  "Effect of D3 on PTH synthesis",
+                  br(),
+                  if (j == 2) {
+                    modal_img(src = "php1_zoom/PTHg/php1_notif_PTHg2.svg", size = "260px")
+                  } else {
+                    modal_img(src = "hypopara_zoom/PTHg/hypopara_notif_PTHg2.svg", size = "260px")
+                  }
+                )
+              ),
+              hr(),
+              fluidRow(
+                column(
+                  width = 6,
+                  align = "center",
+                  "Effect of Pi on PTH synthesis",
+                  br(),
+                  if (j == 2) {
+                    modal_img(src = "php1_zoom/PTHg/php1_notif_PTHg3.svg", size = "260px")
+                  } else {
+                    modal_img(src = "hypopara_zoom/PTHg/hypopara_notif_PTHg3.svg", size = "260px")
+                  }
+                ),
+                column(
+                  width = 6,
+                  align = "center",
+                  "Effect of Ca on PTH secretion",
+                  br(),
+                  if (j == 2) {
+                    modal_img(src = "php1_zoom/PTHg/php1_notif_PTHg4.svg", size = "260px")
+                  } else {
+                    modal_img(src = "hypopara_zoom/PTHg/hypopara_notif_PTHg4.svg", size = "260px")
+                  }
+                )
+              )
+            )
+          }
+        }
+      )
+    })
+  })
+}
 
-# hypoD3
-generate_modal(
-  title = "Detailed Baseline Intestinal Mechanisms",
-  location = "intestine",
-  disease = "hypoD3",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      modal_img("hypoD3_zoom/intestine/hypoD3_notif_intestine.svg")
-    )
-  )
-)
-
-
-#-------------------------------------------------------------------------
-# Bone Mechanisms
-#-------------------------------------------------------------------------
-
-# baseline
-generate_modal(
-  title = "Detailed Baseline Bone Mechanisms",
-  location = "bones",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      "Effect of PTH on bone",
-      br(),
-      modal_img("base_case_zoom/bone/base_case_notif_bone1.svg")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      "Effect of D3 on bone",
-      br(),
-      modal_img("base_case_zoom/bone/base_case_notif_bone2.svg")
-    )
-  )
-)
-
-# php1
-generate_modal(
-  title = "Detailed Baseline Bone Mechanisms",
-  location = "bones",
-  disease = "php1",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      "Effect of PTH on bone",
-      br(),
-      modal_img("php1_zoom/bone/php1_notif_bone1.svg")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      "Effect of D3 on bone",
-      br(),
-      modal_img("php1_zoom/bone/php1_notif_bone2.svg")
-    )
-  )
-)
-
-
-# hypopara
-generate_modal(
-  title = "Detailed Baseline Bone Mechanisms",
-  location = "bones",
-  disease = "hypopara",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      "Effect of PTH on bone",
-      br(),
-      modal_img("hypopara_zoom/bone/hypopara_notif_bone1.svg")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      "Effect of D3 on bone",
-      br(),
-      modal_img("hypopara_zoom/bone/hypopara_notif_bone2.svg")
-    )
-  )
-)
-
-
-# hypoD3
-generate_modal(
-  title = "Detailed Baseline Bone Mechanisms",
-  location = "bones",
-  disease = "hypoD3",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      "Effect of PTH on bone",
-      br(),
-      modal_img("hypoD3_zoom/bone/hypoD3_notif_bone1.svg")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      "Effect of D3 on bone",
-      br(),
-      modal_img("hypoD3_zoom/bone/hypoD3_notif_bone2.svg")
-    )
-  )
-)
-
-
-#-------------------------------------------------------------------------
-# Kidney Mechanisms
-#-------------------------------------------------------------------------
-
-# baseline
-generate_modal(
-  title = "Detailed Baseline Kidney Mechanisms",
-  location = "kidneys",
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca PT reabsorption",
-      br(),
-      modal_img(src = "base_case_zoom/kidney/base_case_notif_kidney1.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Pi PT reabsorption",
-      br(),
-      modal_img(src = "base_case_zoom/kidney/base_case_notif_kidney2.svg", size = "260px")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca TAL reabsorption",
-      br(),
-      modal_img(src = "base_case_zoom/kidney/base_case_notif_kidney3.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca DCT reabsorption",
-      br(),
-      modal_img(src = "base_case_zoom/kidney/base_case_notif_kidney4.svg", size = "260px")
-    )
-  )
-)
-
-
-# php1
-generate_modal(
-  title = "Detailed Baseline Kidney Mechanisms",
-  location = "kidneys",
-  disease = "php1",
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca PT reabsorption",
-      br(),
-      modal_img(src = "php1_zoom/kidney/php1_notif_kidney1.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Pi PT reabsorption",
-      br(),
-      modal_img(src = "php1_zoom/kidney/php1_notif_kidney2.svg", size = "260px")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca TAL reabsorption",
-      br(),
-      modal_img(src = "php1_zoom/kidney/php1_notif_kidney3.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca DCT reabsorption",
-      br(),
-      modal_img(src = "php1_zoom/kidney/php1_notif_kidney4.svg", size = "260px")
-    )
-  )
-)
-
-
-# hypopara
-generate_modal(
-  title = "Detailed Baseline Kidney Mechanisms",
-  location = "kidneys",
-  disease = "hypopara",
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca PT reabsorption",
-      br(),
-      modal_img(src = "hypopara_zoom/kidney/hypopara_notif_kidney1.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Pi PT reabsorption",
-      br(),
-      modal_img(src = "hypopara_zoom/kidney/hypopara_notif_kidney2.svg", size = "260px")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca TAL reabsorption",
-      br(),
-      modal_img(src = "hypopara_zoom/kidney/hypopara_notif_kidney3.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca DCT reabsorption",
-      br(),
-      modal_img(src = "hypopara_zoom/kidney/hypopara_notif_kidney4.svg", size = "260px")
-    )
-  )
-)
-
-
-# hypoD3
-generate_modal(
-  title = "Detailed Baseline Kidney Mechanisms",
-  location = "kidneys",
-  disease = "hypoD3",
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca PT reabsorption",
-      br(),
-      modal_img(src = "hypoD3_zoom/kidney/hypoD3_notif_kidney1.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Pi PT reabsorption",
-      br(),
-      modal_img(src = "hypoD3_zoom/kidney/hypoD3_notif_kidney2.svg", size = "260px")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca TAL reabsorption",
-      br(),
-      modal_img(src = "hypoD3_zoom/kidney/hypoD3_notif_kidney3.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Detailed Ca DCT reabsorption",
-      br(),
-      modal_img(src = "hypoD3_zoom/kidney/hypoD3_notif_kidney4.svg", size = "260px")
-    )
-  )
-)
-
-#-------------------------------------------------------------------------
-# Parathyroid Glands Mechanisms
-#-------------------------------------------------------------------------
-
-# baseline
-generate_modal(
-  title = "Detailed Baseline Parathyroid Gland Mechanisms",
-  location = "PTHg",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      modal_img("base_case_zoom/PTHg/base_case_notif_PTHg.svg")
-    )
-  )
-)
-
-# php1
-generate_modal(
-  title = "Detailed Baseline Parathyroid Gland Mechanisms During Primary-Hyperparathyroidism",
-  location = "PTHg",
-  disease = "php1",
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed PTH mechanisms",
-      br(),
-      modal_img(src = "php1_zoom/PTHg/php1_notif_PTHg1.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Effect of D3 on PTH synthesis",
-      br(),
-      modal_img(src = "php1_zoom/PTHg/php1_notif_PTHg2.svg", size = "260px")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Effect of Pi on PTH synthesis",
-      br(),
-      modal_img(src = "php1_zoom/PTHg/php1_notif_PTHg3.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Effect of Ca on PTH secretion",
-      br(),
-      modal_img(src = "php1_zoom/PTHg/php1_notif_PTHg4.svg", size = "260px")
-    )
-  )
-)
-
-# hypopara
-generate_modal(
-  title = "Detailed Baseline Parathyroid Gland Mechanisms During Hypoparathyroidism",
-  location = "PTHg",
-  disease = "hypopara",
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Detailed PTH mechanisms",
-      br(),
-      modal_img(src = "hypopara_zoom/PTHg/hypopara_notif_PTHg1.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Effect of D3 on PTH synthesis",
-      br(),
-      modal_img(src = "hypopara_zoom/PTHg/hypopara_notif_PTHg2.svg", size = "260px")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      width = 6,
-      align = "center",
-      "Effect of Pi on PTH synthesis",
-      br(),
-      modal_img(src = "hypopara_zoom/PTHg/hypopara_notif_PTHg3.svg", size = "260px")
-    ),
-    br(),
-    column(
-      width = 6,
-      align = "center",
-      "Effect of Ca on PTH secretion",
-      br(),
-      modal_img(src = "hypopara_zoom/PTHg/hypopara_notif_PTHg4.svg", size = "260px")
-    )
-  )
-)
-
-
-# hypoD3
-generate_modal(
-  title = "Detailed Parathyroid Gland Mechanisms During Vitamin D3 Deficiency",
-  location = "PTHg",
-  disease = "hypoD3",
-  fluidRow(
-    column(
-      width = 12,
-      align = "center",
-      "Detailed PTH mechanisms",
-      br(),
-      modal_img(src = "hypoD3_zoom/PTHg/hypoD3_notif_PTHg1.svg", size = "260px")
-    )
-  )
-)
+create_all_modals(diseases = diseases_ids, locations = locations)
